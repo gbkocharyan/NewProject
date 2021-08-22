07954ecc574cee5a248852487ec07cf0276b3584
Gevorg Kocharyan <g.b.kocharyan@gmail.com>
2021-08-22 17:23:28 +0400

    The triangle in another form

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 int main() {
 
     int i = 1;
-    while(i <= 5) {
-        int j = 5;
-        while(j >= i) {
-            printf(" ");
+    while(i < 6) {
+        int j = 6;
+        while(j > i) {
+            printf("  ");
             j--;
         }
         j = 1;
-        while(j <= i) {
+        while(j < 2*i) {
             printf("* ");
             j++;
         }
