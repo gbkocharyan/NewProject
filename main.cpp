2521ed5606123a6c302df01dcf318e36b85c1d22
Gevorg Kocharyan <g.b.kocharyan@gmail.com>
2021-09-04 20:04:17 +0400

    A function that returns the largest element of an array

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
+void max_arr(int length, int arr[]) {
+    int max = 0;
+    for(int i = 0; i < length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    std::cout << max << "\n";
+}
+
 int main() {
 
-    int i = 1;
-    while(i < 6) {
-        int j = 6;
-        while(j > i) {
-            printf("  ");
-            j--;
-        }
-        j = 1;
-        while(j < 2*i) {
-            printf("* ");
-            j++;
-        }
-        i++;
-        printf("\n");
-    }
+    const int length = 4;
+    int array[length] = {21, 3, 52, 19};
+    max_arr(length, array);
+
     return 0;
 }
